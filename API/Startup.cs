5616ef8e9e63cf047d1f5f61b3fab47542425bd8
Martin Michalik <xmicha82@vutbr.cz>
2022-07-14 21:35:38 +0200

    FE skeleton done

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,6 +24,7 @@ namespace API
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
       });
+      services.AddCors();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +40,8 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
       app.UseAuthorization();
 
