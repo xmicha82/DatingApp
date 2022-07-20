7e37435dddf85d19654b60ce1768ecd617824a5e
Martin Michalik <xmicha82@vutbr.cz>
2022-07-20 21:45:53 +0200

    Member cards and details added

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,20 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) {}
 
   login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+    return this.http.post(`${this.baseUrl}/account/login`, model).pipe(
       map((response: User) => {
         const user = response;
         if (user) {
@@ -27,7 +28,7 @@ export class AccountService {
   }
 
   register(model: any) {
-    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+    return this.http.post(`${this.baseUrl}/account/register`, model).pipe(
       map((user: User) => {
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
