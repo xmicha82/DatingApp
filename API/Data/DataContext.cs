b6005c7ec3b14e744956fa06279d2f3504aaad62
Martin Michalik <darkeriossss@DarkeriossssPC.>
2025-01-06 17:24:48 +0100

    BE: DB switch, pre CI

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -50,7 +50,7 @@ public class DataContext(DbContextOptions options) : IdentityDbContext<
       .HasOne(s => s.TargetUser)
       .WithMany(l => l.LikedByUsers)
       .HasForeignKey(s => s.TargetUserId)
-      .OnDelete(DeleteBehavior.Cascade);
+      .OnDelete(DeleteBehavior.NoAction);
 
     builder.Entity<Message>()
       .HasOne(m => m.Recipient)
--- API/Program.cs
+++ API/Program.cs
@@ -31,9 +31,13 @@ app.UseCors(x => x.AllowAnyHeader()
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/messages");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
